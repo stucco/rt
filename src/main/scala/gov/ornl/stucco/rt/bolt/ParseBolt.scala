e8decb5dc7230884916e4fa54dec4a681c47ccff
testak <testakm@ornl.gov>
2013-09-11 12:33:08 -0400

    Added functionality to get the metadata info and match the incoming data to its corresponding Extractor

--- src/main/scala/gov/ornl/stucco/rt/bolt/ParseBolt.scala
+++ src/main/scala/gov/ornl/stucco/rt/bolt/ParseBolt.scala
@@ -24,7 +24,7 @@ import grizzled.slf4j.Logging
  * corresponding subgraph.
  */
 class ParseBolt extends BaseRichBolt with Logging {
-
+  
   private var collector: OutputCollector = _
 
   /**
@@ -34,14 +34,22 @@ class ParseBolt extends BaseRichBolt with Logging {
    */
   def process(uuid: String, json: String): Values = {
     // perform parsing
-    val jsonObj = JsonParser(json) // using morph to get these from the json string...
-    val sourceType = (jsonObj ~> "source" ~> "name").asString
-    val data = (jsonObj ~> "content").asString
+    val message = JsonParser(json)
+    val sourceType : String = (message ~> "source" ~> "name").asString
+    val data : String = (message ~> "content").asString
+    val contentType : String = (message ~> "contentType").asString
+    debug(s"sourceType: $sourceType")
+    debug(s"data: $data")
+    debug(s"contentType: $contentType")
+    //match extractor to data source
     val graph = sourceType match {
       case "cve" => CveExtractor(XmlParser(data)).toString
       case "nvd" => NvdExtractor(XmlParser(data)).toString
-      //TODO: add more cases here
-      case _ => "{ \"edges\":[], \"vertices\":[] }" //TODO: default case, what to do?
+      case "argus" => ArgusExtractor(XmlParser(data)).toString
+      case "cpe" => CpeExtractor(XmlParser(data)).toString
+//      case "hone" => HoneExtractor(CsvParser(data)).toString
+      case "maxmind" => GeoIPExtractor(CsvParser(data)).toString
+      case _ => "{ \"edges\":[], \"vertices\":[] }"
     }
     new Values(uuid, graph)
   }
