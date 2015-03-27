1e36151c8bad492064026cb51fd5a8aa555ed3bc
Shawn Bohn <sjfawks@gmail.com>
2015-03-27 13:07:51 -0700

    setup to add schema and indexes

--- maven-rt-build.sh
+++ maven-rt-build.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 
 echo "Checking out modules..."
-mvn -q --non-recursive scm:checkout -Dmodule.name=JSON-java
-mvn -q --non-recursive scm:checkout -Dmodule.name=morph
-mvn -q --non-recursive scm:checkout -Dmodule.name=html-extractor
-mvn -q --non-recursive scm:checkout -Dmodule.name=extractors
-mvn -q --non-recursive scm:checkout -Dmodule.name=entity-extractor
-mvn -q --non-recursive scm:checkout -Dmodule.name=relation-extractor
-mvn -q --non-recursive scm:checkout -Dmodule.name=graph-alignment
-mvn -q --non-recursive scm:checkout -Dmodule.name=document-service-client-java
+mvn -q --non-recursive scm:checkout -Dmodule.name=JSON-java -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=morph -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=html-extractor -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=extractors -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=entity-extractor -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=relation-extractor -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=graph-alignment -Dbranch.name=schema
+mvn -q --non-recursive scm:checkout -Dmodule.name=document-service-client-java -Dbranch.name=master
+mvn -q --non-recursive scm:checkout -Dmodule.name=ontology -Dbranch.name=forIndexing
 
 echo "Building rt..."
-mvn -q clean install -Dmaven.test.skip=true
+mvn clean install -Dmaven.test.skip=true
 cd streaming-processor
-mvn -q clean package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true
 cd ..
 
+cp graph-alignment/target/populate_schema.jar streaming-processor/target/.
+
 echo "Cleaning up modules..."
-rm -rf JSON-java
-rm -rf morph
-rm -rf html-extractor
-rm -rf extractors
-rm -rf entity-extractor
-rm -rf relation-extractor
-rm -rf graph-alignment
-rm -rf document-service-client-java
+echo "rm -rf JSON-java"
+echo "rm -rf morph"
+echo "rm -rf html-extractor"
+echo "rm -rf extractors"
+echo "rm -rf entity-extractor"
+echo "rm -rf relation-extractor"
+echo "rm -rf graph-alignment"
+echo "rm -rf document-service-client-java"
+echo "rm -rf ontology"
 
