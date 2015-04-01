775b31fe0075e79359b31e05be72653045b53be6
euf <euf@ornl.gov>
2015-04-01 16:04:53 -0400

    update maven build script for alignment repo changes

--- maven-rt-build.sh
+++ maven-rt-build.sh
@@ -1,28 +1,34 @@
 #!/bin/sh
 
 echo "Checking out modules..."
-mvn -q --non-recursive scm:checkout -Dmodule.name=JSON-java
-mvn -q --non-recursive scm:checkout -Dmodule.name=morph
-mvn -q --non-recursive scm:checkout -Dmodule.name=html-extractor
-mvn -q --non-recursive scm:checkout -Dmodule.name=extractors
-mvn -q --non-recursive scm:checkout -Dmodule.name=entity-extractor
-mvn -q --non-recursive scm:checkout -Dmodule.name=relation-extractor
-mvn -q --non-recursive scm:checkout -Dmodule.name=graph-alignment
-mvn -q --non-recursive scm:checkout -Dmodule.name=document-service-client-java
+#graph alignment handling
+mvn --non-recursive scm:checkout -Dmodule.name=graph-alignment -DscmVersion=development -DscmVersionType=branch
+cd graph-alignment
+mvn --non-recursive scm:checkout -Dmodule.name=rexster-client-java
+mvn -q clean install -Dmaven.test.skip=true
+cd ..
+#the rest of the dependencies
+mvn --non-recursive scm:checkout -Dmodule.name=JSON-java
+mvn --non-recursive scm:checkout -Dmodule.name=morph
+mvn --non-recursive scm:checkout -Dmodule.name=html-extractor
+mvn --non-recursive scm:checkout -Dmodule.name=extractors
+mvn --non-recursive scm:checkout -Dmodule.name=entity-extractor
+mvn --non-recursive scm:checkout -Dmodule.name=relation-extractor
+mvn --non-recursive scm:checkout -Dmodule.name=document-service-client-java
 
 echo "Building rt..."
-mvn -q clean install -Dmaven.test.skip=true
+mvn -e clean install -Dmaven.test.skip=true
 cd streaming-processor
-mvn -q clean package -Dmaven.test.skip=true
+mvn -e clean package -Dmaven.test.skip=true
 cd ..
 
-echo "Cleaning up modules..."
-rm -rf JSON-java
-rm -rf morph
-rm -rf html-extractor
-rm -rf extractors
-rm -rf entity-extractor
-rm -rf relation-extractor
-rm -rf graph-alignment
-rm -rf document-service-client-java
+#echo "Cleaning up modules..."
+#rm -rf JSON-java
+#rm -rf morph
+#rm -rf html-extractor
+#rm -rf extractors
+#rm -rf entity-extractor
+#rm -rf relation-extractor
+#rm -rf graph-alignment
+#rm -rf document-service-client-java
 
