40ba8f55c9c0c1d833b0fb6bdec75db4b5e7be94
Maria Vincent <mvincen2@vols.utk.edu>
2016-04-20 20:01:05 -0400

    removed unused dependencies; added STIXExtractors and graph-alignment branch

--- maven-rt-build.sh
+++ maven-rt-build.sh
@@ -5,14 +5,11 @@ mvn -q --non-recursive scm:checkout -Dmodule.name=rexster-client-java
 cd rexster-client-java
 sudo mvn -e clean install -Dmaven.test.skip=true
 cd ..
-mvn --non-recursive scm:checkout -Dmodule.name=JSON-java
-mvn --non-recursive scm:checkout -Dmodule.name=morph
-mvn --non-recursive scm:checkout -Dmodule.name=html-extractor
-mvn --non-recursive scm:checkout -Dmodule.name=extractors
 mvn --non-recursive scm:checkout -Dmodule.name=entity-extractor
 mvn --non-recursive scm:checkout -Dmodule.name=relation-extractor
 mvn --non-recursive scm:checkout -Dmodule.name=document-service-client-java
 mvn --non-recursive scm:checkout -Dmodule.name=graph-alignment
+mvn --non-recursive scm:checkout -Dmodule.name=STIXExtractors
 
 echo "Building rt..."
 mvn -e clean install -Dmaven.test.skip=true
@@ -21,12 +18,9 @@ mvn -e clean package -Dmaven.test.skip=true
 cd ..
 
 #echo "Cleaning up modules..."
-#rm -rf JSON-java
-#rm -rf morph
-#rm -rf html-extractor
-#rm -rf extractors
 #rm -rf entity-extractor
-#rm -rf relation-extractor
+#rm -rf relation-extractors
 #rm -rf graph-alignment
 #rm -rf document-service-client-java
+#rm -rf STIXExtractor
 
