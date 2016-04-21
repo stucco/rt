ab96045be7411f461a8c2db70da2dc63b7487786
euf <euf@ornl.gov>
2016-04-21 15:17:08 -0400

    update pom file and build script, to pull updated dependencies

--- maven-rt-build.sh
+++ maven-rt-build.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 echo "Checking out modules..."
-mvn -q --non-recursive scm:checkout -Dmodule.name=rexster-client-java
-cd rexster-client-java
-sudo mvn -e clean install -Dmaven.test.skip=true
-cd ..
+mvn --non-recursive scm:checkout -Dmodule.name=document-service-client-java
 mvn --non-recursive scm:checkout -Dmodule.name=entity-extractor
 mvn --non-recursive scm:checkout -Dmodule.name=relation-extractor
-mvn --non-recursive scm:checkout -Dmodule.name=document-service-client-java
-mvn --non-recursive scm:checkout -Dmodule.name=graph-alignment
 mvn --non-recursive scm:checkout -Dmodule.name=STIXExtractors
+
+mvn -q --non-recursive scm:checkout -Dmodule.name=graph-db-connection -DscmVersion=ornl-integration -DscmVersionType=branch
+cd graph-db-connection
+mvn -e clean install -Dmaven.test.skip=true
+cd ..
+
+mvn -q --non-recursive scm:checkout -Dmodule.name=graph-alignment -DscmVersion=loadSTIX -DscmVersionType=branch
+cd graph-alignment
+mvn -e clean install -Dmaven.test.skip=true
+cd ..
+
 
 echo "Building rt..."
 mvn -e clean install -Dmaven.test.skip=true
